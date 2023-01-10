7ad498113471fad96302c1ac555119e54112270d
Jorge <jorge.huanca@unmsm.edu.pe>
2023-01-10 10:33:34 -0500

    material_practica_calificada

---
+++ practica_calificada_1/practica_calificada_1.R
@@ -0,0 +1,72 @@
+
+# Información general -----------------------------------------------------
+
+## Curso: Fundamentos de R para CCSS y Gestión Pública
+## Asunto: Práctica Calificada N° 1
+## Grupo: N°6
+## Integrantes: 
+## - HINOJOSA CAHUANA, PERCY ALBERTH
+## - HUANCA MARTINEZ, JORGE ALBERTO
+## - HUANCAYA IDONE, CESAR DANTE
+## - TINTAYA ORIHUELA, MEIR ALVARO
+
+# Pregunta 1 --------------------------------------------------------------
+
+## Ejercicio a ----
+## Abra la data del repositorio GitHub y describa brevemente.
+## Asegurarse que el objeto posea la clase tibble.
+
+
+## Ejercicio b ----
+## Cree una nueva variable (de nombre “fecha”) 
+## usando la variable “FECHA_VACUNACION”.
+## Puede utilizar la función ymd () del paquete “lubridate”
+
+
+## Ejercicio c ----
+## Cree una nueva data que agregue los datos según fecha
+## para saber el total de vacunados por día.
+## ¿Qué día se realizó la mayor cantidad de vacunas en Apurímac?
+
+
+## Ejercicio d ----
+## ¿Cuáles son los tres distritos que tiene una mayor población vacunada
+## con la 3ra dosis?
+
+
+# Pregunta 2 --------------------------------------------------------------
+
+## Ejercicio a ----
+## Abra las siguientes bases de datos GitHub: 
+## WorldCupMatches.csv y WorldCups.csv
+
+
+## Ejercicio b ----
+## Genere una lista con los países donde se ha realizado una copa mundial.
+## Sólo deben figurar valores únicos.
+
+
+## Ejercicio c ----
+## Presente cuál fue el estadio que ha acogido a la mayor cantidad
+## de espectadores o asistentes en la historia de los mundiales
+
+
+## Ejercicio d ----
+## Indique cuál es el referee que más partidos ha arbitrado
+
+
+## Ejercicio e ----
+## Cuál es el partido en el que se anotaron más goles
+
+
+## Ejercicio f ----
+## En la base “WordCupMatches” cree las siguientes nuevas variables:
+## equipo ganador, equipo 2do puesto y equipo 3er puesto
+
+
+## Ejercicio g ----
+## En la base “WorldCups” genere dos columnas que indiquen: 
+## 1) el número de golesanotados por el campeón (primer puesto), y
+## 2) el número de goles anotados por equipo que quedó segundo puesto 
+
+
