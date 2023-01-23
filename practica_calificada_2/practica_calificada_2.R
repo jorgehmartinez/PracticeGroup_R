67af07c884cd6efaebe44dbffcdbec786b850a67
MeirGitHub <121982142+MeirGitHub@users.noreply.github.com>
2023-01-23 07:36:35 -0500

    R - Indicaciones de la practica 2
    Se subió las indicaciones de en archivo R

---
+++ practica_calificada_2/practica_calificada_2.R
@@ -0,0 +1,22 @@
+# Información general -----------------------------------------------------
+
+## Curso: Fundamentos de R para CCSS y Gestión Pública
+## Asunto: Práctica Calificada N° 2
+## Grupo: N°6
+## Integrantes: 
+## - HINOJOSA CAHUANA, PERCY ALBERTH
+## - HUANCA MARTINEZ, JORGE ALBERTO
+## - HUANCAYA IDONE, CESAR DANTE
+## - TINTAYA ORIHUELA, MEIR ALVARO
+
+# Pregunta 1 --------------------------------------------------------------
+## Ejercicio a ----
+
+
+
+## Ejercicio b ----
+
+
+# Pregunta 2 --------------------------------------------------------------
+
+
